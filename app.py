252f58b8ca4c941253a49d57446e855be3a7c8c9
clemlel2 <166614092+clemlel2@users.noreply.github.com>
2024-04-10 13:41:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
+#https://www.trading-attitude.com/comment-lancer-un-script-python-depuis-google-sheets
+# le port local de la web app est http://127.0.0.1:5000
 from flask import Flask
+import subprocess
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def launch():
+    # Specify the path to your .bat or .cmd file
+
+    try:
+        return '{"data": "Hello, World!", "data2": "coucou"}'       
+        
+    
+    except Exception as e:
+        msg = f"An error occurred: {str(e)}"
+        return msg
+    
+    
+if __name__ == '__main__':
+    # Run the Flask application    
+    app.run(debug=True)
