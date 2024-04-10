454dd1e17d1cf125ad7554ff9bea8db424675318
clemlel2 <166614092+clemlel2@users.noreply.github.com>
2024-04-10 14:17:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,17 +12,17 @@ def launch():
     # Specify the path to your .bat or .cmd file
 
     try:
-        mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        port = 9100
-        mysocket.connect(("172.31.40.105", port))
-        contenu = f'''^XA
-    ^XFE:TEMPLATE.ZPL^FS
-    ^FN1^FDJohn^FS
-    ^FN2^FDDoe^FS
-    ^PQ1
-    ^XZ'''.encode()
-        mysocket.send(contenu)
-        mysocket.close() # fermeture de la connexion
+    #     mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #     port = 9100
+    #     mysocket.connect(("172.31.40.105", port))
+    #     contenu = f'''^XA
+    # ^XFE:TEMPLATE.ZPL^FS
+    # ^FN1^FDJohn^FS
+    # ^FN2^FDDoe^FS
+    # ^PQ1
+    # ^XZ'''.encode()
+    #     mysocket.send(contenu)
+    #     mysocket.close() # fermeture de la connexion
         return '{"data": "Version 2"}'       
         
     
